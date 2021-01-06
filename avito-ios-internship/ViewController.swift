2d2a4226e91ce0bf824730937c67e7caeae9c967
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-01-07 02:49:01 +0300

    #1 create project

---
+++ avito-ios-internship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito-ios-internship
+//
+//  Created by Георгий Куликов on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
