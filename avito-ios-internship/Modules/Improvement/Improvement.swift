c05eb061a37b0cf59a249ea9243b4893d679c54f
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-01-07 03:39:07 +0300

    #1 add entities

--- avito-ios-internship/Modules/Improvement/Improvement.swift
+++ avito-ios-internship/Modules/Improvement/Improvement.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct ImprovementIcon: Codable {
+    var url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case url = "52x52"
+    }
+}
+
+class Improvement: Codable {
+    var id: String
+    var title: String
+    var description: String?
+    var icon: ImprovementIcon
+    var price: String
+    var isSelected: Bool
+}
+
+struct AdvertismentImprovement: Codable {
+    var title: String
+    var actionTitle: String
+    var selectedActionTitle: String
+    var improvements: [Improvement]
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case actionTitle
+        case selectedActionTitle
+        case improvements = "list"
+    }
+}
+
+struct StatusData: Codable {
+    var status: String
+    var result: AdvertismentImprovement
+}
