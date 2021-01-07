7e1774247185b67b24d77c22635d7efeabfccf6e
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-01-07 03:41:16 +0300

    #1 add interactor logic

--- avito-ios-internship/Modules/Improvement/ImprovementInteractor.swift
+++ avito-ios-internship/Modules/Improvement/ImprovementInteractor.swift
@@ -7,10 +7,86 @@
 
 import UIKit
 
-protocol ImprovementInteractorProtocol {
+protocol ImprovementInteractorProtocol: class {
+    func improvementChanged(at index: Int)
+    func improvementsCount() -> Int?
     
+    func getImprovementTitle(at index: Int) -> String?
+    func getImprovementDescription(at index: Int) -> String?
+    func getImprovementPrice(at index: Int) -> String?
+    func getImprovementIconURL(at index: Int) -> String?
+    func isImprovementSelected(at index: Int) -> Bool?
+    
+    func getTitle() -> String?
+    func getActionTitle() -> String?
+    func getSelectedImprovementTitle() -> String?
+    func getSelectedActionTitle() -> String?
 }
 
 class ImprovementInteractor: ImprovementInteractorProtocol {
-
+    weak var presenter: ImprovementPresenterProtocol?
+    
+    private var advertismentImprovement: AdvertismentImprovement?
+    private var selectedImprovement: Improvement?
+    
+    private var loader: LoaderProtocol = FileLoader()
+    private var parser: ImprovementParserProtocol = ImprovementJSONParser()
+    
+    init() {
+        guard let jsonImprovementData = loader.load(from: "result") else { return }
+        advertismentImprovement = parser.parse(from: jsonImprovementData)
+        let improvements = advertismentImprovement?.improvements
+        if let selected = improvements?.filter({ $0.isSelected }), selected.count > 0 {
+            selectedImprovement = selected.first
+        }
+    }
+    
+    func improvementChanged(at index: Int) {
+        guard let improvement = advertismentImprovement?.improvements[index] else { return }
+        improvement.isSelected = !improvement.isSelected
+        if let currentImprovement = selectedImprovement {
+            currentImprovement.isSelected = false
+        }
+        selectedImprovement = improvement
+    }
+    
+    func getImprovementTitle(at index: Int) -> String? {
+        return advertismentImprovement?.improvements[index].title
+    }
+    
+    func getImprovementDescription(at index: Int) -> String? {
+        return advertismentImprovement?.improvements[index].description
+    }
+    
+    func getImprovementPrice(at index: Int) -> String? {
+        return advertismentImprovement?.improvements[index].price
+    }
+    
+    func getImprovementIconURL(at index: Int) -> String? {
+        return advertismentImprovement?.improvements[index].icon.url
+    }
+    
+    func isImprovementSelected(at index: Int) -> Bool? {
+        return advertismentImprovement?.improvements[index].isSelected
+    }
+    
+    func improvementsCount() -> Int? {
+        return advertismentImprovement?.improvements.count
+    }
+    
+    func getTitle() -> String? {
+        return advertismentImprovement?.title
+    }
+    
+    func getActionTitle() -> String? {
+        return advertismentImprovement?.actionTitle
+    }
+    
+    func getSelectedImprovementTitle() -> String? {
+        return selectedImprovement?.title
+    }
+    
+    func getSelectedActionTitle() -> String? {
+        return advertismentImprovement?.selectedActionTitle
+    }
 }
